da60e659b75e2c3a61e5e248807d1c0d776677d0
Vanglorian <vanglorianengenharia@gmail.com>
2025-11-04 22:54:40 -0300

    Corrige comportamento do click no menu em telas touch

--- src/components/01_header_navigation/HeaderNavigation.tsx
+++ src/components/01_header_navigation/HeaderNavigation.tsx
@@ -5,6 +5,7 @@ import styles from "./HeaderNavigation.module.css";
 import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
+import { usePathname } from "next/navigation";
 
 export function HeaderNavigation() {
   const[menuOpen, setMenuOpen] = useState(false)
@@ -13,14 +14,19 @@ export function HeaderNavigation() {
     const whatsappLink = `https://wa.me/${phoneNumber}?text=Olá!%20Acessei%20o%20%20site%20da%20Vanglorian%20e%20gostaria%20de%20mais%20informações.`;
     window.open(whatsappLink, '_blank'); 
   }
+  
+  const pathname= usePathname();
+  console.log(pathname);
+  
 
   return(
     <header className={styles.wrapperHeaderNav} aria-label="Cabeçalho principal do site">
       <div className={styles.containerHeaderNav}>
         <div
           className={styles.menuSandwichBlock}
-          onMouseEnter={() => setMenuOpen(true)}
-          onMouseLeave={() => setMenuOpen(false)}
+          onMouseEnter={() => !menuOpen && setMenuOpen(true)}
+          onMouseLeave={() => menuOpen && setMenuOpen(false)}
+          onClick={() => {setMenuOpen(!menuOpen)}}
         >
           <button className={styles.menuSandwichBtn}>
             <Menu color="#002100" size={19} />
@@ -32,9 +38,10 @@ export function HeaderNavigation() {
                 <li className={`${styles.modalMenuItem} ${styles.modalMenuItem1}`}>
                   <Link href="/">Página Inicial</Link>
                 </li>
-                <li className={`${styles.modalMenuItem} ${styles.modalMenuItem2}`}>
-                  <Link href="#execBlock">Em execução</Link>
-                </li>
+
+                {pathname === '/emExecucao' ? (<span className={styles.modalMenuItemSpan}>Em execução</span>) :  (<li className={`${styles.modalMenuItem} ${styles.modalMenuItem2}`}>
+                  <Link href="/emExecucao">Em execução</Link></li>)}
+
                 <li className={`${styles.modalMenuItem} ${styles.modalMenuItem3}`}>
                   <Link href="#lancamentBlock">Lançamentos</Link>
                 </li>
@@ -58,7 +65,9 @@ export function HeaderNavigation() {
         </div> 
         <nav className={styles.nav} aria-label="Navegação principal">
           <ul className={styles.menu}>
-            <li><Link href="#execBlock" className={`${styles.menuItem} ${styles.menuItem1}`}>Em execução</Link></li>
+
+            {pathname === '/emExecucao' ? (<span className={styles.menuItemSpan}>Em execução</span>) : (<li><Link href="/emExecucao" rel="noopener noreferrer" className={`${styles.menuItem} ${styles.menuItem1}`}>Em execução</Link></li>)}
+
             <li><Link href="#lancamentBlock" className={`${styles.menuItem} ${styles.menuItem2}`}>Lançamentos</Link></li>
             <li><button className={`${styles.menuItem} ${styles.menuItem3}`} onClick={handleClickTalkWithUs}>Fale conosco</button></li>
           </ul>
