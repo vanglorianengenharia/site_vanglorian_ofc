a54c050432d33aa285c9016a07a5080142f8c66c
Vanglorian <vanglorianengenharia@gmail.com>
2026-04-17 16:26:00 -0300

    Corrige espaço a mais na mensagem de whatsapp do botão Agendar visita.

--- src/app/residencial-v-falatian-casas-3-4/page.tsx
+++ src/app/residencial-v-falatian-casas-3-4/page.tsx
@@ -226,7 +226,7 @@ const IconLavanderia = currentLavanderia.icon
 
     const phoneNumber = '+554191459026'; 
     const handleClickTalkWithUs = () => {
-    const whatsappLink = `https://wa.me/${phoneNumber}?text=Olá!%20Gostei%20do%20%20residencial%20V-Falatian%20e%20gostaria%20de%20agendar%20uma%20visita.`;
+    const whatsappLink = `https://wa.me/${phoneNumber}?text=Olá!%20Gostei%20do%20residencial%20V-Falatian%20e%20gostaria%20de%20agendar%20uma%20visita.`;
     window.open(whatsappLink, '_blank'); 
   }
   return (
